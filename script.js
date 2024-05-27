128daa9da651895cfd3990555d6d20a7e3cf3fb5
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-05-27 23:28:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function checkLeapYear(year) {
 
     //three conditions to find out the leap year
     if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
-        console.log(year + ' is a leap year');
+        return 366;
     } else {
-        console.log(year + ' is not a leap year');
+        return 365;
     }
 }
 
 // take input
-const year = prompt('daysOfAYear('+year);
-
+const year = prompt('Enter a year:');
+console.log(daysOfAYear(year));
 checkLeapYear(year);
